dff69959db41dd5a86f237ef5c23c5a5d8e8d272
martinnguyen-it <martin.nguyentam@gmail.com>
2024-05-08 16:35:31 +0700

    auto set lower case email when user request login

--- app/domain/auth/entity.py
+++ app/domain/auth/entity.py
@@ -1,9 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 from app.domain.admin.entity import Admin
 from app.domain.shared.entity import BaseEntity
 from app.domain.student.entity import Student
+from app.shared.utils.general import transform_email
 
 
 class Token(BaseModel):
@@ -18,6 +19,7 @@ class TokenData(BaseModel):
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+    _extract_email = field_validator("email", mode="before")(transform_email)
 
 
 class AuthAdminInfoInResponse(BaseEntity, Token):
