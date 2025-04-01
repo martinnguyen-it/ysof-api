00977be3decf260248e7e1893d4de81f0986b8ea
martinnguyen-it <martin.nguyentam@gmail.com>
2025-04-01 22:56:48 +0700

    refactor: update EmailSMTPService with headers and SSL for lower spam score

--- app/infra/email/email_smtp_service.py
+++ app/infra/email/email_smtp_service.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.utils import make_msgid, formataddr
 import pytz
 from app.config import settings
 from app.infra.logging import get_logger
+import ssl
 
 logger = get_logger()
 
@@ -14,25 +16,23 @@ class EmailSMTPService:
         self, emails_to: list[str] | str, subject: str, plain_text: str, html: str | None = None
     ):
         msg = MIMEMultipart("alternative")
-        msg["From"] = f"YSOF <{settings.YSOF_EMAIL_SENDER}>"
-        msg["To"] = emails_to
+        msg["From"] = formataddr(("YSOF", settings.YSOF_EMAIL_SENDER))
+        msg["To"] = emails_to if isinstance(emails_to, str) else ", ".join(emails_to)
         msg["Subject"] = subject
+        msg["Message-ID"] = make_msgid(domain=settings.SMTP_MAIL_HOST)
         current_time = datetime.now(pytz.timezone(settings.TIMEZONE))
-        formatted_date = current_time.strftime("%a, %d %b %Y %H:%M:%S %z")
-
-        msg["Date"] = formatted_date
-        msg["reply-to"] = settings.YSOF_EMAIL_SENDER
+        msg["Date"] = current_time.strftime("%a, %d %b %Y %H:%M:%S %z")
+        msg["Reply-To"] = settings.YSOF_EMAIL_SENDER
+        msg["List-Unsubscribe"] = f"<mailto:{settings.YSOF_EMAIL_SENDER}>"
 
         msg.attach(MIMEText(plain_text, "plain"))
         if html:
             msg.attach(MIMEText(html, "html"))
 
-        service = smtplib.SMTP(host=settings.SMTP_MAIL_HOST, port=settings.SMTP_MAIL_PORT)
-        service.starttls()
-        service.login(user=settings.SMTP_MAIL_USER, password=settings.SMTP_MAIL_PASSWORD)
-
-        res = service.send_message(msg)
-        service.quit()
+        with smtplib.SMTP(host=settings.SMTP_MAIL_HOST, port=settings.SMTP_MAIL_PORT) as service:
+            service.starttls(context=ssl.create_default_context())
+            service.login(user=settings.SMTP_MAIL_USER, password=settings.SMTP_MAIL_PASSWORD)
+            res = service.send_message(msg)
         return res
 
     def send_email_welcome(self, email: str, plain_text: str):
